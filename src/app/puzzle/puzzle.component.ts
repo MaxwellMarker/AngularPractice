63a35da3d34c3b6aeff324d92aca2c71aa0ff0b0
Max Marker <maxmarker98@gmail.com>
2020-11-09 22:37:03 -0500

    made some changes to allow swapping between different columns

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,51 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-puzzle',
   templateUrl: './puzzle.component.html',
-  styleUrls: ['./puzzle.component.scss']
+  styleUrls: ['./puzzle.component.scss'],
 })
 export class PuzzleComponent implements OnInit {
+  one = ['three', 'two', 'one', 'four'];
+  two = ['four', 'two', 'five', 'three'];
+  three = ['four', 'three', 'six', 'five'];
+  four = ['five', 'seven', 'six', 'four'];
+  constructor() {}
 
-  one = [
-    "three",
-    "two",
-    "one",
-    "four",
-  ]
-  two = [
-    "four",
-    "two",
-    "five",
-    "three",
-  ]
-  three = [
-    "four",
-    "three",
-    "six",
-    "five",
-  ]
-  four = [
-    "five",
-    "seven",
-    "six",
-    "four",
-  ]
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      setTimeout(() => {
+        transferArrayItem(
+          event.container.data,
+          event.previousContainer.data,
+          event.currentIndex + 1,
+          event.previousIndex,
+        );
+      }, 100);
     }
   }
-
 }
